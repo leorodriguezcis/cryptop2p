75368385bbbf09cd8dd9cd97940cd05b2c98e09f
Leonardo Rodriguez <lrodriguez@practia.global>
2023-03-24 16:10:57 -0300

    java proyect

---
+++ src/main/java/ar/edu/unq/grupoA/backendcryptop2p/BackendCryptop2pApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.grupoA.backendcryptop2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCryptop2pApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCryptop2pApplication.class, args);
+	}
+
+}
