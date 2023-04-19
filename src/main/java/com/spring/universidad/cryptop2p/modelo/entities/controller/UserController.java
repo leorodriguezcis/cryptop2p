4e247fc47846a7815384d363e0b6556bd13864fe
Leonardo Rodriguez <lrodriguez@practia.global>
2023-04-19 17:34:28 -0300

    agregado swagger

--- src/main/java/com/spring/universidad/cryptop2p/modelo/entities/controller/UserController.java
+++ src/main/java/com/spring/universidad/cryptop2p/modelo/entities/controller/UserController.java
@@ -1,17 +1,21 @@
 package com.spring.universidad.cryptop2p.modelo.entities.controller;
 
 import com.spring.universidad.cryptop2p.services.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.spring.universidad.cryptop2p.modelo.entities.dto.UserRegisterDto;
 import javax.validation.Valid;
-
+import io.swagger.annotations.Api;
 @RestController
+@Api(tags = "Register")
 public class UserController {
     @Autowired
     private UserService userService;
-    
+
+    @ApiOperation(value = "register user")
     @PostMapping(value="/api/auth/register")
     public ResponseEntity<String> userRegister(@Valid @RequestBody UserRegisterDto user){
         System.out.println(user.getName());
