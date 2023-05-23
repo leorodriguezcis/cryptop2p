ac9ec17b1f5b02284a6839020b650dfc017f76ce
leorodriguezcis <54456767+leorodriguezcis@users.noreply.github.com>
2023-05-23 10:14:20 -0300

    Update UniversidadBackendApplication.java

--- src/main/java/com/spring/universidad/cryptop2p/UniversidadBackendApplication.java
+++ src/main/java/com/spring/universidad/cryptop2p/UniversidadBackendApplication.java
@@ -7,7 +7,7 @@ public class UniversidadBackendApplication {
 
 
 	public static void main(String[] args) {
-		String[] str = SpringApplication.run(UniversidadBackendApplication.class, args).getBeanDefinitionNames();
+		 SpringApplication.run(UniversidadBackendApplication.class, args).getBeanDefinitionNames();
 	}
 
 }
