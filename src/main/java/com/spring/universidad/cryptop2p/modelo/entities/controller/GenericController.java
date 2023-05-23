537991beb00245556037873fec06fededbd0282d
leorodriguezcis <54456767+leorodriguezcis@users.noreply.github.com>
2023-05-23 10:30:51 -0300

    Update GenericController.java

--- src/main/java/com/spring/universidad/cryptop2p/modelo/entities/controller/GenericController.java
+++ src/main/java/com/spring/universidad/cryptop2p/modelo/entities/controller/GenericController.java
@@ -1,12 +1,6 @@
 package com.spring.universidad.cryptop2p.modelo.entities.controller;
 
 import com.spring.universidad.cryptop2p.services.interfaces.GenericDAO;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class GenericController<E, S extends GenericDAO<E>>{
 
