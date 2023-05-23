765c8829a85539f5b50f397042d55fe72f6a4f98
leorodriguezcis <54456767+leorodriguezcis@users.noreply.github.com>
2023-05-23 10:18:01 -0300

    Update User.java

--- src/main/java/com/spring/universidad/cryptop2p/modelo/entities/User.java
+++ src/main/java/com/spring/universidad/cryptop2p/modelo/entities/User.java
@@ -27,11 +27,11 @@ public class User implements Serializable {
     @Column(name = "user_wallet")
     protected String wallet;
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
-    public Set<Transaction> listTransactions;
+    private Set<Transaction> listTransactions;
     @Column(name = "user_reputation")
-    public Integer reputation;
+    private Integer reputation;
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
-    public Set<Transaction> transactions;
+    private Set<Transaction> transactions;
 
     public User(String name, String lastname, String email, String address, String password, String cvu, String wallet ){
         this.name = name;
