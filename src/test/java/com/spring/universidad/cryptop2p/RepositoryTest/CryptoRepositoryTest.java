85a7c4a722295f27283300110460415a78070fff
Leonardo Rodriguez <54456767+leorodriguezcis@users.noreply.github.com>
2023-06-25 20:07:53 -0300

    Update CryptoRepositoryTest.java

--- src/test/java/com/spring/universidad/cryptop2p/RepositoryTest/CryptoRepositoryTest.java
+++ src/test/java/com/spring/universidad/cryptop2p/RepositoryTest/CryptoRepositoryTest.java
@@ -14,7 +14,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-public class CryptoRepositoryTest {
+class CryptoRepositoryTest {
     @Autowired
     CryptoRepository cryptoRepository;
 
