367997fb9a8cbefe2d7c71e49cad1ed0f2cdd3ce
Leonardo Rodriguez <lrodriguez@practia.global>
2023-06-24 21:08:55 -0300

    fixed tests

--- src/test/java/com/spring/universidad/cryptop2p/ServiceTest/TransactionServiceTest.java
+++ src/test/java/com/spring/universidad/cryptop2p/ServiceTest/TransactionServiceTest.java
@@ -3,6 +3,7 @@ package com.spring.universidad.cryptop2p.ServiceTest;
 import com.spring.universidad.cryptop2p.data.DatosDummy;
 import com.spring.universidad.cryptop2p.model.dto.TransactionDTO;
 import com.spring.universidad.cryptop2p.model.entities.Transaction;
+import com.spring.universidad.cryptop2p.model.entities.User;
 import com.spring.universidad.cryptop2p.model.enums.CryptoEnum;
 import com.spring.universidad.cryptop2p.model.enums.TransactionState;
 import com.spring.universidad.cryptop2p.model.repository.CryptoRepository;
@@ -39,12 +40,12 @@ public class TransactionServiceTest {
     public void setUp(){
         trans1 = new TransactionDTO("sell", CryptoEnum.ETHUSDT,1d,879000d,"chaco lopez",new BigDecimal(11831) );
         transactionService = new TransactionService(transactionRepository, userRepository, cryptoRepo);
-        userRepository.save(DatosDummy.getUser1());//ID 3
-        userRepository.save(DatosDummy.getUser2());//ID 4
+        userRepository.save(DatosDummy.getUser1());
+        userRepository.save(DatosDummy.getUser2());
         cryptoRepo.save(DatosDummy.getETHUSDT());
         cryptoRepo.save(DatosDummy.getAXSUSDT());
-        
-    }
+        }
+
     @AfterEach
     public void tearDown(){
         userRepository.deleteAll();
@@ -55,7 +56,7 @@ public class TransactionServiceTest {
     @Test
     @DisplayName("add transaction")
     void addTransaction() {
-        transactionService.addTransaction(trans1, 3);
+        transactionService.addTransaction(trans1, 6);
         Iterable<Transaction> res = transactionRepository.findAll();
         ArrayList<Transaction> resList = (ArrayList<Transaction>)res;
         assertThat(resList).hasSize(1);
@@ -64,16 +65,12 @@ public class TransactionServiceTest {
     @Test
     @DisplayName("buy an intention active")
     void buyAnIntentionTest() {
-        transactionService.addTransaction(trans1, 3);
-        System.out.println("ASD"+((ArrayList<Transaction>)transactionRepository.findAll()).get(0).getId());
-        transactionService.buyAnIntention(3, 2 );
-        Optional<Transaction> res = transactionRepository.findById(2);
+        transactionService.addTransaction(trans1, 7);
+        transactionService.buyAnIntention(8, 11 );
+        Optional<Transaction> res = transactionRepository.findById(11);
         Transaction resTransaction = res.get();
         assertThat(resTransaction.getIsActive()).isEqualTo(TransactionState.ON_PROCESS);
     }
-
-  
-  
 
 }
      
