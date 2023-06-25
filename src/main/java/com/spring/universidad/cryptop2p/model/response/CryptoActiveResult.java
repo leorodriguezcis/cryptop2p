598c803cfef3130c95fe0a76cef3aaff6535775f
Leonardo Rodriguez <lrodriguez@practia.global>
2023-06-25 20:30:22 -0300

    cryptoresult

--- src/main/java/com/spring/universidad/cryptop2p/model/response/CryptoActiveResult.java
+++ src/main/java/com/spring/universidad/cryptop2p/model/response/CryptoActiveResult.java
@@ -6,38 +6,52 @@ import com.spring.universidad.cryptop2p.model.entities.Transaction;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class CryptoActiveResult implements Serializable {
     LocalDateTime date;
     double volumeDollar;
     double volumeARS;
-    ArrayList<CryptoActive> cryptoActiveList;
-    public CryptoActiveResult(ArrayList<Transaction> transactions ){
+    private List<CryptoActive> cryptoActiveList;
+    public CryptoActiveResult(List<Transaction> transactions ){
         this.date = LocalDateTime.now();
         this.volumeARS = this.getTotalValueArs(transactions);
         this.volumeDollar = this.getTotalDollar(transactions);
         this.cryptoActiveList =  getCryptoActiveList(transactions);
     }
-    public ArrayList<CryptoActive> getCryptoActiveList(ArrayList<Transaction> transactions){
-        ArrayList<CryptoActive> resList = new ArrayList<>();
+    public List<CryptoActive> getCryptoActiveList(List<Transaction> transactions){
+        List<CryptoActive> resList = new ArrayList<>();
         for (Transaction transaction : transactions){
             resList.add(new CryptoActive(transaction));
         }
         return resList;
     }
-    public double getTotalDollar(ArrayList<Transaction> transactions ){
+    public double getTotalDollar(List<Transaction> transactions ){
         double counterDollar = 0;
         for (Transaction transaction : transactions){
             counterDollar += transaction.getNominalValue()* Double.valueOf(transaction.getValueCotization().toString());
         }
         return counterDollar;
     }
-    public double getTotalValueArs(ArrayList<Transaction> transactions ){
+    public double getTotalValueArs(List<Transaction> transactions ){
         double counterARS = 0;
         for (Transaction transaction : transactions){
             counterARS += transaction.getValuePesos();
         }
         return counterARS;
     }
+    public LocalDateTime getDate() {
+        return date;
+    }
+    public double getVolumeDollar() {
+        return volumeDollar;
+    }
+    public double getVolumeARS() {
+        return volumeARS;
+    }
+    public List<CryptoActive> getCryptoActiveList() {
+        return cryptoActiveList;
+    }
+    
 
 }
