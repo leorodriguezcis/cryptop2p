64d871a3e6e4e78fe977039c5a038649c3484e15
Leonardo Rodriguez <lrodriguez@practia.global>
2023-06-26 00:18:39 -0300

    log4j configurations

--- src/main/java/com/spring/universidad/cryptop2p/model/controller/UserController.java
+++ src/main/java/com/spring/universidad/cryptop2p/model/controller/UserController.java
@@ -10,17 +10,25 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import io.swagger.annotations.Api;
 import java.util.Map;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @RestController
 @Api(tags = "User")
 public class UserController {
+    private static final Logger logger = LogManager.getLogger(UserController.class);
+
     @Autowired
     UserService service;
 
     @ApiOperation(value = "register user")
     @PostMapping(value="/register")
     public ResponseEntity<String> userRegister(@Valid @RequestBody UserRegisterDTO user){
+        long startTime = System.nanoTime();
         service.registerUser(user);
+        long endTime = System.nanoTime();
+        long executionTime = endTime - startTime;
+        logger.info("Se creo un nuevo usuario: {}. Tiempo de ejecucion: {} milisegundos", user.getName(), executionTime/1000000);
         return ResponseEntity.ok(user.getName());
     }
 
