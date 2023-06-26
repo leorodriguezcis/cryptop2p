c07f3e58e328a5f5d5a1ca8ccbbfe91fddec12e0
Leonardo Rodriguez <lrodriguez@practia.global>
2023-06-26 16:28:43 -0300

    Test

---
+++ src/test/java/com/architecture/ArchitectureTest.java
@@ -0,0 +1,42 @@
+package com.architecture;
+
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+public class ArchitectureTest {
+    private JavaClasses importedClasses;
+
+    @BeforeEach
+    public void setup() {
+        importedClasses = new ClassFileImporter()
+                .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+                .importPackages("com.spring.universidad.cryptop2p.model");
+    }
+    @Test
+    void serviceClassesShouldOnlyBeAccessedByController() {
+        classes()
+                .that().resideInAPackage("..service..")
+                .should().onlyBeAccessed().byAnyPackage("..service..", "..controller..")
+                .check(importedClasses);
+    }
+    @Test
+    void serviceClassesShouldContainSufixService(){
+        classes()
+                .that().resideInAPackage("..service..")
+                .should().haveSimpleNameEndingWith("Service");
+    }
+    @Test
+    void repositoryClassesShouldContainSufixRepository(){
+        classes()
+                .that().resideInAPackage("..repository..")
+                .should().haveSimpleNameEndingWith("Repository");
+    }
+    
+    
+
+}
--- src/test/java/com/spring/universidad/cryptop2p/ServiceTest/UserServiceTest.java
+++ src/test/java/com/spring/universidad/cryptop2p/ServiceTest/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.spring.universidad.cryptop2p.ServiceTest;
 
+import com.spring.universidad.cryptop2p.model.dto.UserLoginDTO;
 import com.spring.universidad.cryptop2p.model.dto.UserRegisterDTO;
 import com.spring.universidad.cryptop2p.model.entities.User;
 import com.spring.universidad.cryptop2p.model.repository.UserRepository;
@@ -14,10 +15,13 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @DataJpaTest
 class UserServiceTest {
+    private UserLoginDTO userLogin;
     private UserRegisterDTO userTest;
     private UserService userService ;
     
@@ -26,8 +30,9 @@ class UserServiceTest {
 
     @BeforeEach
     public void setUp(){
-         userTest = new UserRegisterDTO("chaco","lopez","chaaaco@gmail.com","123","1234","1234567891234567891234","asdf");
-         userService = new UserService(userRepository);
+        userLogin = new UserLoginDTO("chaco","1234Asd");
+        userTest = new UserRegisterDTO("chaco","lopez","chaaaco@gmail.com","123","1234Asd","1234567891234567891234","asdf");
+        userService = new UserService(userRepository);
     }
     @AfterEach
     public void tearDown(){
@@ -35,10 +40,21 @@ class UserServiceTest {
     }
 
     @Test
-    @DisplayName("test1")
-    void test1() {
+    @DisplayName("register user")
+    void registerTest() {
         userService.registerUser(userTest);
         assertThat(((ArrayList<User>)userRepository.findAll()).get(0).getName()).isEqualTo("chaco");
+    }
+    @Test
+    @DisplayName("login user")
+    void loginTest() {
+        userService.registerUser(userTest);
+        System.out.println("ASD"+userRepository.findByName("chaco"));
+        Map<String, Object> message = userService.logIn(userLogin);
+        Map<String, Object> messageRes = new HashMap<>(); 
+        messageRes.put("SUCCESS", Boolean.TRUE);
+        Object success = message.get("SUCCESS");
+        assertThat(success).isEqualTo(messageRes.get("SUCCESS"));
     }
 }
      
